a39c0544807e85d14fff6c2bd923523d5fce46db
aaronjcurtis <aaronjcurtis@gmail.com>
2018-06-16 16:48:25 -0600

    corrections

--- assignments/lesson-8/scripts/windchill.js
+++ assignments/lesson-8/scripts/windchill.js
@@ -6,14 +6,9 @@
 
 
 function doInputOutput () {
-console.log('Hello');
 var temp = document.getElementById('hightemp').innerHTML;
-console.log(temp);
 var windSpeed = document.getElementById('windspeed').innerHTML;
-console.log(windspeed);
-console.log(temp + windSpeed + 'Hello');
 var itsCold = windChill(temp,windSpeed);
-
 document.getElementById ('demo2').innerHTML = itsCold;
 //	div.innerHTML = itsCold;
 }
@@ -24,8 +19,6 @@ var a = Math.pow(v,.16);
 var itFeelsLike = (35.74 + (0.6215 * t) - (35.75* a) + (.4275 * t) * a).toFixed(2);
 //I got the formula from www.onlineconversion.com/windchill.htm.  
 //I verified the formula from a few separate sources as well
-
-console.log(itFeelsLike);
 return itFeelsLike;
 
 }
